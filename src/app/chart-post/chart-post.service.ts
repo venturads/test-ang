3df381c1dd001590a6e982df07e9c7da060c8a0a
Albert Ventura <albert@Shankars-MBP.fios-router.home>
2018-06-11 22:29:30 -0700

    add timer

--- src/app/chart-post/chart-post.service.ts
+++ src/app/chart-post/chart-post.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
- import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ChartPostService {
   constructor(private http: HttpClient) {}
   fetchClient(): Observable<Object> {
-    return this.http.get('https://ang-api-venturads.c9users.io/posts.json');
-   // return this.http.get('https://codepen.io/jobs.json');
+    return this.http.get('https://rails-api-venturads.c9users.io/posts.json');
+  //  return this.http.get('https://rails-api-venturads.c9users.io/posts/1.json');
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChartPostService } from '../chart-post/chart-post.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/observable/timer';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeComponent  {
+    @Input() name: any;
+    alive = true;
+    public now: Date = new Date();
+    
+    constructor(private appTest : ChartPostService) {
+      Observable.timer(0,5000)
+      .takeWhile(() => this.alive) // only fires when component is alive
+      .subscribe(() => {
+        this.appTest.fetchClient().subscribe(data=> {
+          this.now = new Date();
+          console.log(data[1].name + this.now.getSeconds());
+        })
+      });
+    }
+  
+    ngOnDestroy(){
+      this.alive = false; // switches your IntervalObservable off
+    }
   }
-
-}
